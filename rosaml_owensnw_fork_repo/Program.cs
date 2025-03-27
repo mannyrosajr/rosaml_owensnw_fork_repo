c213791c9265abbd540cc5f03249b9cbd59e19f1
Manny Rosa <mannyrosajr04@outlook.com>
2025-03-27 11:45:23 -0400

    Add project files.

---
+++ rosaml_owensnw_fork_repo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rosaml_owensnw_fork_repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Price = 275000;  
+            myCar.Name = "Lamborgini Urus";
+            Console.WriteLine("The name of the car is " + myCar.Name);
+            Console.WriteLine("Price of my Car is " + myCar.Price); 
+            
+        }
+    }
+}
---
+++ rosaml_owensnw_fork_repo/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rosaml_owensnw_fork_repo
+{
+   /// <summary>
+   /// Manny's favorite car 
+   /// </summary>
+    internal class Car
+    {
+        private String _name;
+        private double _price;
+
+        public double Price { get { return _price; } set { _price = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+    }
+}
